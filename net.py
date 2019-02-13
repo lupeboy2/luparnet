81d2425ba17c219510875bfbf4ec503a9097315d
lupeboy2 <nicklupe13@gmail.com>
2019-02-13 09:51:14 -0500

    Update net.py

--- net.py
+++ net.py
@@ -3,13 +3,13 @@ import numpy as np
 import time
 np.set_printoptions(suppress=True)
 np.warnings.filterwarnings('ignore')
-
+#
 class net():
-    def __init__(self,layer, ds):
+    def __init__(self,layer,ds,function="sigmoid"):
 
         self.layers = layer
         self.d=ds
-        
+        self.func=function
         #seed
         np.random.seed(1)
         syn=[]
@@ -31,44 +31,49 @@ class net():
         syn.append(2*np.random.random((self.d[1],self.d[2])) - 1)
         self.syn=syn
 
-    #define sigmoid
-    def sigmoid(self,toSig, deriv=False):
-        if(deriv==True):
-            return (toSig*(1-toSig))
-        
-        return 1/(1+np.exp(-toSig))
+    #define activate
+    def activate(self,toSig,deriv=False):
+        if(deriv):
+            if(self.func=="sigmoid"):
+                return (toSig*(1-toSig))
+            if(self.func=="arctan"):
+                return 1/(1+x**2)
+            if(self.func=="softplus"):
+                return 1/(1+np.exp(-toSig))
+        if(self.func=="sigmoid"):
+            return 1/(1+np.exp(-toSig))
+        if(self.func=="arctan"):
+            return np.arctan(toSig)
+        if(self.func == "softplus"):
+            return  np.log(1+np.exp(toSig)) 
 
     #define predict
-    def predict(self,a,string=False):
+    def predict(self,data):
         c=0
-        ph2=a
+        ph2=data
         while(c<self.layers):
             ph=ph2
-            ph2=self.sigmoid(np.dot(ph, self.syn[c]))
+            ph2=self.activate(np.dot(ph, self.syn[c]))
             c+=1
-        if(string):
-            data = a
-            dataaaa='\n'.join('\t'.join('%0.3f' %x for x in y) for y in data)
-
-            returnMe=''
-            returnMe+="Prediction for data set: %s"%(dataaaa)
-            returnMe+='\n'+str(float(ph2))
-            ph2=returnMe
         return ph2
 
     #train
-    def train(self,run,a,b,error=True):
+    def train(self,run,indata,outdata,error=False):
+        err = error
+        if type(error) is int:
+            err=True
         for j in xrange(run+1):
             #setup
             t0 = time.clock()
             syn=self.syn
+
             # predictulate forward through the network.
-            l=[a]
+            l=[indata]
             l_error=[]
             l_delta=[]
             c=0
             while c<self.layers:
-                l.append(self.sigmoid(np.dot(l[c], syn[c])))
+                l.append(self.activate(np.dot(l[c], syn[c])))
                 l_error.append(0)
                 l_delta.append(0)
                 c+=1
@@ -78,15 +83,15 @@ class net():
             
             # Back propagation of errors using the chain rule.
 
-            l_error[c]=b-l[c]
-            if error and (j % 5000==0):   # Only print the error every 5000 steps
+            l_error[c]=outdata-l[c]
+            if err and (j % error==0):   # Only print the error every 5000 steps
                 print "Epoch:%d, Loss: %.8f, Time: %.5fs" % (j,(np.mean(np.abs(l_error[c]))), time.clock()-t0)
-            l_delta[c]=l_error[c]*self.sigmoid(l[c], deriv=True)
+            l_delta[c]=l_error[c]*self.activate(l[c], deriv=True)
             c-=1
             
             while c>0:
                 l_error[c]=l_delta[c+1].dot(syn[c].T)
-                l_delta[c]=l_error[c]*self.sigmoid(l[c], deriv=True)
+                l_delta[c]=l_error[c]*self.activate(l[c], deriv=True)
                 c-=1
                 
             #update weights
